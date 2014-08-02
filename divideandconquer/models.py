6d3cd339a9f3d6eb5f21b10f219463e154ba2497
Ian Kronquist <iankronquist@teleport.com>
2014-08-02 14:10:20 -0700

    Add models

---
+++ divideandconquer/models.py
@@ -0,0 +1,26 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = "users"
+    uid = Column(String, primary_key=True)
+    name = Column(String)
+    password = Column(String)
+    salt = Column(String)
+
+class Response(Base):
+    __tablename__ = "responses"
+    # is_spam is a trinary value
+    # 1 is true
+    # 2 is false
+    # 3 is unknown
+    is_spam = Column(Integer)
+    json_hash = Column(String, primary_key=True)
+    json = Column(String)
+
+# this is probably a bad idea...
+class JobQueue(Base):
+    __tablename__ = "jobqueue"
+    json_hash = Column(String, primary_key=True)
+    priority = Column(Integer)
