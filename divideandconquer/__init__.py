c5e73c09d0547e5c111d3fb53b60dbc2a778cadd
Ian Kronquist <iankronquist@teleport.com>
2014-08-02 12:39:28 -0700

    Goodbye redis
    You were so easy to use, it's a shame Heroku wanted money for you

--- divideandconquer/__init__.py
+++ divideandconquer/__init__.py
@@ -10,38 +10,50 @@ import redis
 import requests
 app = Flask(__name__)
 app.debug = True
-r_server = redis.Redis("localhost")
-print 'redis up'
+store = {
+    'queue': [],
+}
 
 @app.route('/', methods=['GET', 'POST'])
 def classify():
+    if len(store.get('queue')) == 0:
+        refillQueue()
     if request.method == 'POST':
-        jresp = r_server.get(request.form['id'])
+        jresp = store.get(request.form['id'])
+        if jresp == None:
+            print 'Error: json data not found'
+            return show_message()
         resp = json.loads(jresp)
         if request.form.get('spam', None):
             resp['spam'] = True
         else:
             resp['spam'] = False
         jresp = json.dumps(resp)
-        r_server.set(request.form['id'], jresp)
-    if r_server.llen('queue') == 0:
-        refillQueue()
-    responseId = r_server.lpop('queue')
-    jsonResponse = r_server.get(responseId)
+        store[request.form['id']] =  jresp
+    return show_message()
+
+
+def show_message():
+    responseId = store['queue'].pop()
+    jsonResponse = store.get(responseId)
     response = json.loads(jsonResponse)
     return render_template('classify.html', description=response['description'],
         happy=response['happy'], id=responseId)
 
+def dump_to_file():
+    fout = open('out.json', 'w')
+    json.dump(store, fout)
+
 def refillQueue():
+    dump_to_file()
     url = "https://input.mozilla.org/api/v1/feedback/?locales=en-US"
     r = requests.get(url)
     jr = r.json()
     for resp in jr['results']:
-        r_server.lpush('queue', resp['id'])
-        r_server.lpush('all', resp['id'])
-        r_server.set(resp['id'], json.dumps(resp))
+        store['queue'].append(resp['id'])
+        store[resp['id']] =  json.dumps(resp)
 
 
-#if __name__ == '__main__':
-#    app.run()
-#    print 'app running'
+if __name__ == '__main__':
+    print 'app running'
+    app.run(debug=True, port=33507)
